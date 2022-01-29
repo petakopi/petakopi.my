017e6cd2b7552725a2fce3e5884b98da1a821f4c
Amree Zaid <mohd.amree@gmail.com>
2022-01-30 07:27:32 +0800

    Disable some generators when scaffolding

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Petakopi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.generators do |generate|
+      generate.helper false
+      generate.test_framework false # Enable this once we are ready to add specs ;)
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
