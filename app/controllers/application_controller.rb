65f559001795a8d00b9115478f94cea2cece786f
Amree Zaid <mohd.amree@gmail.com>
2022-02-05 02:30:18 +0800

    Remove http password, time to open to public 🎉

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :http_authenticate
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
-
-  def http_authenticate
-    return true unless Rails.env == "production"
-
-    authenticate_or_request_with_http_basic do |username, password|
-      # Congrats, you found the password. Feel free to browse the website ;)
-      username == "petakopi" && password == "kopimung"
-    end
   end
 end
