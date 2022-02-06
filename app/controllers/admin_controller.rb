fdcbd00ae527b3e561e3cbcaf02b48f8e2e1d805
Amree Zaid <mohd.amree@gmail.com>
2022-02-06 18:02:46 +0800

    Replace is_admin with role

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -4,6 +4,6 @@ class AdminController < ApplicationController
   def verify_admin_access
     return redirect_to root_path if current_user.nil?
 
-    redirect_to root_path unless current_user.is_admin?
+    redirect_to root_path unless current_user.staff?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,16 @@ class User < ApplicationRecord
     :validatable
 
   validates :name, uniqueness: true
+
+  def admin?
+    role == "admin"
+  end
+
+  def moderator?
+    role == "moderator"
+  end
+
+  def staff?
+    admin? || moderator?
+  end
 end
