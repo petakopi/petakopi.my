a22c24dec06339780c3f5b7889f2e30e36548ca7
Amree Zaid <mohd.amree@gmail.com>
2022-02-07 21:39:17 +0800

    Attempt to fix image link for social sharing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = {host: "petakopi.my", protocol: "https"}
 end
