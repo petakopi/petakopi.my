2ed87f9089b35650bb9ef53019a925edb1c3fc4a
Amree Zaid <mohd.amree@gmail.com>
2022-02-07 21:49:47 +0800

    Remove unknown changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,6 +90,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-  config.action_mailer.default_url_options = {host: "petakopi.my", protocol: "https"}
 end
