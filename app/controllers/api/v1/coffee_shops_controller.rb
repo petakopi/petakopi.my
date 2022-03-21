51626cbb7382c8913cce09f3a7052436d1db5192
Amree Zaid <mohd.amree@gmail.com>
2022-03-21 23:13:44 +0800

    Cache json map

--- app/controllers/api/v1/coffee_shops_controller.rb
+++ app/controllers/api/v1/coffee_shops_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::CoffeeShopsController < ApplicationController
   def index
     @coffee_shops = CoffeeShop.status_published.where.not(lat: nil, lng: nil)
+
+    headers["Cache-Control"] = "max-age=#{60.minute.to_i}, public"
   end
 end
