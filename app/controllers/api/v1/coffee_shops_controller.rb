6b3ac6ba81bdb6e98486d55dca7823032326a888
Amree Zaid <mohd.amree@gmail.com>
2022-03-23 03:49:10 +0800

    Cache query result for map

--- app/controllers/api/v1/coffee_shops_controller.rb
+++ app/controllers/api/v1/coffee_shops_controller.rb
@@ -1,12 +1,18 @@
 class Api::V1::CoffeeShopsController < ApplicationController
   def index
     @coffee_shops =
-      CoffeeShop
-        .includes(logo_attachment: :blob)
-        .status_published
-        .where
-        .not(lat: nil, lng: nil)
-        .select(:id, :name, :slug, :lat, :lng)
+      Rails.cache.fetch(
+        ["coffee_shops", "api"],
+        expires_in: 1.hour,
+        race_condition_ttl: 15.seconds
+      ) do
+        CoffeeShop
+          .includes(logo_attachment: :blob)
+          .status_published
+          .where
+          .not(lat: nil, lng: nil)
+          .select(:id, :name, :slug, :lat, :lng)
+      end
 
     headers["Cache-Control"] = "max-age=#{60.minute.to_i}, public"
   end
