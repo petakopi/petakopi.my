f22a35fc11303322200ac4d873c49940d3b48265
Amree Zaid <mohd.amree@gmail.com>
2022-04-07 22:02:37 +0800

    Raise error if user not found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(username: params[:id])
+    @user = User.find_by!(username: params[:id])
     @submitted_coffee_shops =
       @user
       .submitted_coffee_shops
