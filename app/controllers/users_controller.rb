ea82d1d579a67c878ed832adc11341ab37a48c25
Amree Zaid <mohd.amree@gmail.com>
2022-04-10 07:06:15 +0800

    Require login first before editing user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
+
   def show
     @user = User.find_by!(username: params[:id])
     @favourites =
