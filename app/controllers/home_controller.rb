9ef6f7a540ca3b967e1e47a4d45cb2273edd3af5
Amree Zaid <mohd.amree@gmail.com>
2022-05-08 19:10:37 +0800

    Add verified badge for coffee shop managed by an owner

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
     @coffee_shops =
       CoffeeShopsListQuery.call(
         params: params,
-        relation: CoffeeShop.includes(:tags, logo_attachment: :blob)
+        relation: CoffeeShop.includes(:tags, :owners, logo_attachment: :blob)
       )
     @coffee_shops = @coffee_shops.status_published
 
