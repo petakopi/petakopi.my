609469dd7bc42a03826975fb6717ac8449811b54
Amree Zaid <mohd.amree@gmail.com>
2022-05-08 17:57:55 +0800

    Remove previous moved content to Home#index

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -2,14 +2,6 @@ class CoffeeShopsController < ApplicationController
   before_action :set_coffee_shop, only: %i[show]
 
   def index
-    @coffee_shops =
-      CoffeeShopsListQuery.call(
-        params: params,
-        relation: CoffeeShop.includes(:tags, logo_attachment: :blob)
-      )
-    @coffee_shops = @coffee_shops.status_published
-
-    @pagy, @coffee_shops = pagy(@coffee_shops, items: 20)
   end
 
   def show
