c5aa8c2d9fe0d2bdc6f7de01e707fca06bd910bf
Amree Zaid <mohd.amree@gmail.com>
2022-06-05 00:11:59 +0800

    Use CloudFlare for geocoding

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -7,7 +7,13 @@ Ahoy.api = true
 # set to true for geocoding (and add the geocoder gem to your Gemfile)
 # we recommend configuring local geocoding as well
 # see https://github.com/ankane/ahoy#geocoding
-Ahoy.geocode = true
-Ahoy.job_queue = :default
+Ahoy.geocode = false
 
 Ahoy.server_side_visits = :when_needed
+
+class Ahoy::Store < Ahoy::DatabaseStore
+  def track_visit(data)
+    data[:country] = request.headers["HTTP_CF_IPCOUNTRY"]
+    super(data)
+  end
+end
