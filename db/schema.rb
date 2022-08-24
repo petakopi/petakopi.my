0594fa9a7c429986289f7f5eab757a520ac466db
Amree Zaid <mohd.amree@gmail.com>
2022-08-24 14:59:51 +0800

    Add position for tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_23_232142) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_065748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -165,6 +165,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_23_232142) do
     t.datetime "updated_at", null: false
     t.string "description"
     t.string "group"
+    t.integer "position"
     t.index ["name"], name: "index_tags_on_name", unique: true
     t.index ["slug"], name: "index_tags_on_slug", unique: true
   end
