50b8ace7af0417772878ab4137cc0d1fbf25cd0e
Amree Zaid <mohd.amree@gmail.com>
2023-01-07 10:15:57 +0800

    Expose turbo_native_app helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :turbo_native_app?
+
   protected
 
   def configure_permitted_parameters
