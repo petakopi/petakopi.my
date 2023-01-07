1b72ff344cb1e65731b9776ab804b568e01666e1
Amree Zaid <mohd.amree@gmail.com>
2023-01-07 10:52:03 +0800

    Allow full screen map for turbo native

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,4 +1,14 @@
 class MapController < ApplicationController
+  layout :current_layout
+
   def index
+  end
+
+  private
+
+  def current_layout
+    return "application_full" if turbo_native_app?
+
+    "application"
   end
 end
