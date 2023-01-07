7e898011a5d4adc8d29779a3a0d01f2ba4d49d8e
Amree Zaid <mohd.amree@gmail.com>
2023-01-07 10:18:15 +0800

    Add the ability to return back to the original page once logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
 
   helper_method :turbo_native_app?
 
@@ -11,4 +12,23 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
+
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+  def after_sign_in_path_for(resource_or_scope)
+    if turbo_native_app?
+      turbo_recede_historical_location_path
+    else
+      request.env["omniauth.origin"] ||
+        stored_location_for(resource_or_scope) ||
+        super
+    end
+  end
 end
+
