0851966d0d1300f4d476f58f729f3fedd4389949
Amree Zaid <mohd.amree@gmail.com>
2023-04-02 13:51:08 +0800

    Add last 7 days check ins

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -9,6 +9,12 @@ class CoffeeShopsController < ApplicationController
   def show
     @coffee_shop = ActiveDecorator::Decorator.instance.decorate(@coffee_shop)
 
+    last_7_days_check_ins = CheckIn.includes(:user).where(created_at: 7.days.ago.., coffee_shop: @coffee_shop)
+    @check_ins = {
+      count: last_7_days_check_ins.count,
+      users: last_7_days_check_ins.order(created_at: :desc).limit(4).map(&:user)
+    }
+
     ahoy.track "View Coffee Shop", id: @coffee_shop.id, name: @coffee_shop.name
   end
 
