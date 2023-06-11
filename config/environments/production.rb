e9a725d7ec43722288f6dde29871de0e127e553f
Amree Zaid <mohd.amree@gmail.com>
2023-06-11 10:43:43 +0800

    Update production mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,17 +68,11 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :postmark
   config.action_mailer.default_url_options = {host: "petakopi.my"}
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    domain: "gmail.com",
-    port: 587,
-    user_name: "petakopi.my@gmail.com",
-    password:  Rails.application.credentials.dig(:smtp_password),
-    authentication: "plain",
-    enable_starttls_auto: true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.postmark_settings = {
+    api_token: Rails.application.credentials.postmark_api_token
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
