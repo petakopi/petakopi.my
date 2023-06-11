6dd45086e9864169d4df003ea5517f5fda1fe733
Amree Zaid <mohd.amree@gmail.com>
2023-06-11 10:49:54 +0800

    Fix sender email

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -38,7 +38,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "petakopi.my@gmail.com"
+  config.mailer_sender = "hello@petakopi.my"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
