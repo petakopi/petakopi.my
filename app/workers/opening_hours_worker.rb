7cd1f135c1c487adb27ddb961f28e17e76c0e05b
Amree Zaid <mohd.amree@gmail.com>
2023-06-18 09:22:39 +0800

    Add API throttler since I don't want to be homeless

--- app/workers/opening_hours_worker.rb
+++ app/workers/opening_hours_worker.rb
@@ -4,6 +4,10 @@ class OpeningHoursWorker < SidekiqWorker
   def perform(coffee_shop_id)
     coffee_shop = CoffeeShop.find(coffee_shop_id)
 
+    if PaidApiThrottler.block_update_opening_hours?(coffee_shop)
+      return
+    end
+
     if coffee_shop.google_place_id.blank?
       GooglePlaceIdProcessor.call(coffee_shop: coffee_shop)
     end
