ba6005cfc9fe4be1edb63c15c602e58257e7d41d
Amree Zaid <mohd.amree@gmail.com>
2023-11-11 22:15:51 +0800

    Hide PWA install button for iOS and Safari

--- app/javascript/controllers/pwa_install_controller.js
+++ app/javascript/controllers/pwa_install_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 import ahoy from "ahoy.js"
 
 export default class extends Controller {
+  static targets = ["button"]
+
+  connect() {
+    if (this._isIOS() || this._isSafari()) {
+      this.buttonTarget.classList.add("hidden")
+    }
+  }
+
   install() {
     // Show the Prompt
     window.deferredPrompt.prompt();
@@ -9,10 +17,18 @@ export default class extends Controller {
     window.deferredPrompt.userChoice.then((choiceResult) => {
       ahoy.track(
         "PWA",
-        {accepted: choiceResult.outcome === 'accepted'}
+        {accepted: choiceResult.outcome === "accepted"}
       )
 
       window.deferredPrompt = null;
     });
+  }
+
+  _isIOS() {
+    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
+  }
+
+  _isSafari() {
+    return /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
   }
 }
