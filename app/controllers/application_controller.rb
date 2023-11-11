ded84d29a80cfe6e1057ced7b6a4151428733187
Amree Zaid <mohd.amree@gmail.com>
2023-11-11 18:16:47 +0800

    Fix user unable to register due to username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
   def storable_location?
