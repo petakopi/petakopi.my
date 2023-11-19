db1940118429e60de05584da5d6d1f4f3d87f6b1
Amree Zaid <mohd.amree@gmail.com>
2023-11-19 10:59:15 +0800

    Fix 404 error when submitting new coffee shop

--- app/controllers/coffee_shops_controller.rb
+++ app/controllers/coffee_shops_controller.rb
@@ -24,7 +24,7 @@ class CoffeeShopsController < ApplicationController
 
     respond_to do |format|
       if @coffee_shop.save
-        format.html { redirect_to coffee_shop_url(@coffee_shop), notice: success_message }
+        format.html { redirect_to new_coffee_shop_path, notice: success_message }
         format.json { render :show, status: :created, location: @coffee_shop }
       else
         format.html { render :new, status: :unprocessable_entity }
