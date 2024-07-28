2d515a96fc6aa38b51f4e79c140ec1ca2c1733e7
Amree Zaid <mohd.amree@gmail.com>
2024-07-28 18:55:44 +0800

    Use help from turnstile gem

--- config/initializers/cloudflare_turnstile.rb
+++ config/initializers/cloudflare_turnstile.rb
@@ -1,8 +1,16 @@
 RailsCloudflareTurnstile.configure do |c|
-  c.enabled = Rails.application.credentials.dig(:cloudflare, :turnstile, :site_key).present?
-  c.site_key = Rails.application.credentials.dig(:cloudflare, :turnstile, :site_key)
-  c.secret_key = Rails.application.credentials.dig(:cloudflare, :turnstile, :secret_key)
+  # Ref: https://developers.cloudflare.com/turnstile/troubleshooting/testing/
+  if Rails.env.production?
+    c.site_key = Rails.application.credentials.dig(:cloudflare, :turnstile, :site_key)
+    c.secret_key = Rails.application.credentials.dig(:cloudflare, :turnstile, :secret_key)
+  else
+    c.site_key = "1x00000000000000000000AA"
+    c.secret_key = "1x0000000000000000000000000000000AA"
+  end
+
+  c.theme = :light
   c.fail_open = true
+  c.enabled = Rails.application.credentials.dig(:cloudflare, :turnstile, :site_key).present?
 end
 
 module RailsCloudflareTurnstileTurboPatch
