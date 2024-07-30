3119ed3d464882f6d18bde108abf7177d8fd2b01
Amree Zaid <mohd.amree@gmail.com>
2024-07-30 23:06:20 +0800

    Replaces Telegram secrets with ENV

--- app/workers/telegram_notifier_worker.rb
+++ app/workers/telegram_notifier_worker.rb
@@ -2,6 +2,9 @@ require "telegram/bot"
 
 class TelegramNotifierWorker < SidekiqWorker
   def perform(message)
+    bot_token = ENV["TELEGRAM_BOT_TOKEN"]
+    notification_chat_id = ENV["TELEGRAM_NOTIFICATION_CHAT_ID"]
+
     Telegram::Bot::Client.run(Rails.application.credentials[:telegram][:bot_token]) do |bot|
       bot.api.send_message(chat_id: Rails.application.credentials[:telegram][:notification_chat_id], text: message)
     end
