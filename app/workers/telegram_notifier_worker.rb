980ba9d2cda87fbab8e1b7d8f141e42a5759dc5c
Amree Zaid <mohd.amree@gmail.com>
2024-08-13 08:28:39 +0800

    Fix Telegram token

--- app/workers/telegram_notifier_worker.rb
+++ app/workers/telegram_notifier_worker.rb
@@ -5,8 +5,8 @@ class TelegramNotifierWorker < SidekiqWorker
     bot_token = ENV["TELEGRAM_BOT_TOKEN"]
     notification_chat_id = ENV["TELEGRAM_NOTIFICATION_CHAT_ID"]
 
-    Telegram::Bot::Client.run(Rails.application.credentials[:telegram][:bot_token]) do |bot|
-      bot.api.send_message(chat_id: Rails.application.credentials[:telegram][:notification_chat_id], text: message)
+    Telegram::Bot::Client.run(bot_token) do |bot|
+      bot.api.send_message(chat_id: notification_chat_id, text: message)
     end
   end
 end
