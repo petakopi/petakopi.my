e09813dbe42b53dde9e1f30afe0bb7d74af9482b
Amree Zaid <mohd.amree@gmail.com>
2025-03-09 07:40:24 +0800

    Use helper to decide on the image link

---
+++ app/helpers/assets_helper.rb
@@ -0,0 +1,16 @@
+module AssetsHelper
+  def optimized_blob_url(asset:, env: Rails.env, options: [])
+    blob_path = rails_public_blob_url(asset)
+
+    if env.production?
+      [
+        "https://assets.petakopi.my",
+        "cdn-cgi/image",
+        options.join(","),
+        blob_path
+      ].join("/")
+    else
+      blob_path
+    end
+  end
+end
