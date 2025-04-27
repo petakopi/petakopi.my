03f25f34f303d28e67b1ea6c3f10190b6833c968
Amree Zaid <mohd.amree@gmail.com>
2025-04-27 09:16:35 +0800

    Fix error with the button inside of another button

--- app/javascript/components/Home/FilterComponents/FilterCategory.jsx
+++ app/javascript/components/Home/FilterComponents/FilterCategory.jsx
@@ -13,20 +13,21 @@ const FilterCategory = ({ title, isOpen, setIsOpen, count = 0, hasInfo = false, 
             {title}
             {hasInfo && (
               <span className="inline-flex items-center ml-1">
-                <button
+                <div
                   ref={infoButtonRef}
-                  type="button"
                   onClick={(e) => {
                     e.stopPropagation();
                     onInfoClick();
                   }}
-                  className="text-gray-400 hover:text-gray-500 focus:outline-none inline-flex items-center"
+                  className="text-gray-400 hover:text-gray-500 focus:outline-none inline-flex items-center cursor-pointer"
+                  role="button"
+                  tabIndex={0}
                   aria-label="Information"
                 >
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                   </svg>
-                </button>
+                </div>
               </span>
             )}
           </span>
