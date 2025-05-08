d4c3823cca59c23a06f4eaa8fabac7fe02b5e81e
Amree Zaid <mohd.amree@gmail.com>
2025-05-09 06:52:09 +0800

    Fix search

--- app/javascript/utils/filters.js
+++ app/javascript/utils/filters.js
@@ -1,5 +1,10 @@
 // Helper function to add filters to URL
 export const applyFiltersToUrl = (url, filters) => {
+  // Add keyword filter if it exists
+  if (filters.keyword) {
+    url.searchParams.append('keyword', filters.keyword);
+  }
+
   // Add tags filter if it exists (multiple tags)
   if (filters.tags && filters.tags.length > 0) {
     filters.tags.forEach(tag => {
@@ -7,9 +12,24 @@ export const applyFiltersToUrl = (url, filters) => {
     });
   }
 
+  // Add state filter if it exists
+  if (filters.state) {
+    url.searchParams.append('state', filters.state);
+  }
+
+  // Add district filter if it exists
+  if (filters.district) {
+    url.searchParams.append('district', filters.district);
+  }
+
   // Add opened filter if it exists
   if (filters.opened) {
     url.searchParams.append('opened', filters.opened);
+  }
+
+  // Add distance filter if it exists
+  if (filters.distance) {
+    url.searchParams.append('distance', filters.distance);
   }
 
   return url;
