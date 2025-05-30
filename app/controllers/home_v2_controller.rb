882ab4792ba05e8b6476f7ffc9955f1562e7d5bf
Amree Zaid <mohd.amree@gmail.com>
2025-05-30 08:45:21 +0800

    Fix for guests

--- app/controllers/home_v2_controller.rb
+++ app/controllers/home_v2_controller.rb
@@ -2,6 +2,15 @@ class HomeV2Controller < ApplicationController
   DEFAULT_ITEMS_PER_PAGE = 20
 
   def index
-    @collections = current_user.collections.select(:id, :name).order(:name)
+    @collections =
+    if current_user
+      current_user
+        .collections
+        .select(:id, :name)
+        .order(:name)
+        .limit(10)
+    else
+      []
+    end
   end
 end
